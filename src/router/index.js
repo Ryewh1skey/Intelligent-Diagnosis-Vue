f5ce1b9bbcfa83a1ffe921317aa34303a3b13c41
Lijiaxi127 <1092911409@qq.com>
2023-06-16 22:17:49 +0800

    layout功能更改

--- src/router/index.js
+++ src/router/index.js
@@ -49,32 +49,23 @@ export const constantRoutes = [
     redirect: '/dashboard',
     children: [{
       path: 'dashboard',
-      name: 'Dashboard',
+      name: '主页',
       component: () => import('@/views/dashboard/index'),
-      meta: { title: 'Dashboard', icon: 'dashboard' }
+      meta: { title: '主页', icon: 'dashboard' }
     }]
   },
 
   {
     path: '/example',
     component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: 'Example', icon: 'el-icon-s-help' },
+    redirect: '/example/',
     children: [
       {
         path: 'table',
-        name: 'Table',
+        name: '病例列表',
         component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
-      }
-    ]
+        meta: { title: '病例列表', icon: 'el-icon-s-help' }
+      }]
   },
 
   {
@@ -83,9 +74,9 @@ export const constantRoutes = [
     children: [
       {
         path: 'index',
-        name: 'Form',
+        name: '添加用户',
         component: () => import('@/views/form/index'),
-        meta: { title: 'Form', icon: 'form' }
+        meta: { title: '添加用户', icon: 'form' }
       }
     ]
   },
@@ -94,71 +85,67 @@ export const constantRoutes = [
     path: '/nested',
     component: Layout,
     redirect: '/nested/menu1',
-    name: 'Nested',
-    meta: {
-      title: 'Nested',
-      icon: 'nested'
-    },
     children: [
       {
         path: 'menu1',
         component: () => import('@/views/nested/menu1/index'), // Parent router-view
-        name: 'Menu1',
-        meta: { title: 'Menu1' },
-        children: [
-          {
-            path: 'menu1-1',
-            component: () => import('@/views/nested/menu1/menu1-1'),
-            name: 'Menu1-1',
-            meta: { title: 'Menu1-1' }
-          },
-          {
-            path: 'menu1-2',
-            component: () => import('@/views/nested/menu1/menu1-2'),
-            name: 'Menu1-2',
-            meta: { title: 'Menu1-2' },
-            children: [
-              {
-                path: 'menu1-2-1',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-                name: 'Menu1-2-1',
-                meta: { title: 'Menu1-2-1' }
-              },
-              {
-                path: 'menu1-2-2',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-                name: 'Menu1-2-2',
-                meta: { title: 'Menu1-2-2' }
-              }
-            ]
-          },
-          {
-            path: 'menu1-3',
-            component: () => import('@/views/nested/menu1/menu1-3'),
-            name: 'Menu1-3',
-            meta: { title: 'Menu1-3' }
-          }
-        ]
-      },
-      {
-        path: 'menu2',
-        component: () => import('@/views/nested/menu2/index'),
-        name: 'Menu2',
-        meta: { title: 'menu2' }
+        name: '设置',
+        meta: { title: '设置', icon: 'nested' }
+
+        //   children: [
+        //     {
+        //       path: 'menu1-1',
+        //       component: () => import('@/views/nested/menu1/menu1-1'),
+        //       name: 'Menu1-1',
+        //       meta: { title: 'Menu1-1' }
+        //     },
+        //     {
+        //       path: 'menu1-2',
+        //       component: () => import('@/views/nested/menu1/menu1-2'),
+        //       name: 'Menu1-2',
+        //       meta: { title: 'Menu1-2' },
+        //       children: [
+        //         {
+        //           path: 'menu1-2-1',
+        //           component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
+        //           name: 'Menu1-2-1',
+        //           meta: { title: 'Menu1-2-1' }
+        //         },
+        //         {
+        //           path: 'menu1-2-2',
+        //           component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
+        //           name: 'Menu1-2-2',
+        //           meta: { title: 'Menu1-2-2' }
+        //         }
+        //       ]
+        //     },
+        //     {
+        //       path: 'menu1-3',
+        //       component: () => import('@/views/nested/menu1/menu1-3'),
+        //       name: 'Menu1-3',
+        //       meta: { title: 'Menu1-3' }
+        //     }
+        //   ]
+        // },
+        // {
+        //   path: 'menu2',
+        //   component: () => import('@/views/nested/menu2/index'),
+        //   name: 'Menu2',
+        //   meta: { title: 'menu2' }
       }
     ]
   },
 
-  {
-    path: 'external-link',
-    component: Layout,
-    children: [
-      {
-        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-        meta: { title: 'External Link', icon: 'link' }
-      }
-    ]
-  },
+  // {
+  //   path: 'external-link',
+  //   component: Layout,
+  //   children: [
+  //     {
+  //       path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
+  //       meta: { title: 'External Link', icon: 'link' }
+  //     }
+  //   ]
+  // },
 
   // 404 page must be placed at the end !!!
   { path: '*', redirect: '/404', hidden: true }
@@ -173,7 +160,7 @@ const createRouter = () => new Router({
 const router = createRouter()
 
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
-export function resetRouter() {
+export function resetRouter () {
   const newRouter = createRouter()
   router.matcher = newRouter.matcher // reset router
 }
